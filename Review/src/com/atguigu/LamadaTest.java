ce70931cafa4d196dc3de5e2cc551cd06aa2a7ae
kp-null <kp_null@163.com>
2020-11-04 16:38:46 +0800

    test 2

--- Review/src/com/atguigu/LamadaTest.java
+++ Review/src/com/atguigu/LamadaTest.java
@@ -2,6 +2,8 @@ package com.atguigu;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class LamadaTest {
@@ -13,6 +15,10 @@ public class LamadaTest {
         System.out.println("更新");
         System.out.println("二次更新");
         System.out.println("三次更新");
+        System.out.println("四次更新");
+        List<Integer> integers = Arrays.asList(1, 23, 4567, 34);
+        integers.forEach(System.out::println);
+
 
     }
 
